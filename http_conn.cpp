39808c380fd36ca62308b4dd13a2b2f39de200ea
Doraemonwei <1470028580@qq.com>
2023-07-27 13:38:02 +0000

    请求首行的解析

--- http_conn.cpp
+++ http_conn.cpp
@@ -17,7 +17,7 @@ void setnonblocking(int fd){
 void addfd(int epollfd,int fd, bool one_shot){
     epoll_event event;
     event.data.fd=fd;
-    event.events = EPOLLIN | EPOLLRDHUP;
+    event.events = EPOLLIN | EPOLLRDHUP|EPOLLET;
     if(one_shot){
         event.events|EPOLLONESHOT;
     }
@@ -53,7 +53,24 @@ void http_conn::init(int sockfd, const sockaddr_in &addr){
     // 添加到epoll对象中，就不用再main中写了
     addfd(m_epollfd,m_sockfd,true);
     m_user_count++; // 用户数目加1
+    init();
 }
+
+// 初始化连接其余的数据
+void http_conn::init(){
+    m_check_state = CHECK_STATE_REQUESTLINE; // 主状态机的初始化状态为解析请求首行
+    m_checked_index = 0; // 当前正在解析的行的起始位置
+    m_start_line = 0;
+
+    m_read_index = 0;
+    m_method  =GET;
+    m_url=0;
+    m_version=0;
+    m_linger=false;
+    m_host=0;
+    bzero(m_read_buf,READ_BUFFER_SIZE);
+}
+
 
 // 关闭连接
 void http_conn::close_conn(){
@@ -67,20 +84,173 @@ void http_conn::close_conn(){
 
 // 非阻塞读取数据
 bool http_conn::read(){
-    printf("一次性读完了数据\n");
+    if(m_read_index>=READ_BUFFER_SIZE){
+        return false;
+    }
+    
+    // 读取到的字节
+    int bytes_read=0;
+    while(true){
+        bytes_read = recv(m_sockfd, m_read_buf+m_read_index, READ_BUFFER_SIZE-m_read_index, 0);
+        if(bytes_read==-1){
+            if(errno==EAGAIN||errno==EWOULDBLOCK){
+                // 没有数据
+                break;
+            }
+            return false;
+        }else if(bytes_read==0){
+            // 对方关闭连接
+            return false;
+        }
+        m_read_index+=bytes_read;
+    }
+    printf("读取到了数据:%s\n",m_read_buf);
     return true;
 }
 // 非阻塞写数据
 bool http_conn::write(){
     printf("一次性写完了数据\n");
     return true;
-
 }
-
 
 // 响应并处理客户端的请求,处理http请求的入口函数
 void http_conn::process(){
     // 解析http请求
+    HTTP_CODE read_ret = process_read();
+    if(read_ret==NO_REQUEST){
+        modfd(m_epollfd,m_sockfd,EPOLLIN);
+        return;
+    }
     printf("HTTP请求解析...\n");
     // 生成响应
 }
+
+// 解析请求信息
+http_conn::HTTP_CODE http_conn::process_read(){
+    // 主状态机
+    LINE_STATUS line_status = LINE_OK;
+    HTTP_CODE ret = NO_REQUEST;
+    char* text = 0;
+    // 一行一行的解析
+    while(((m_start_line==CHECK_STSTE_CONTENT)&& (line_status==LINE_OK))||((line_status=parse_line())==LINE_OK)){
+        text = get_line(); // 获取一行数据
+        m_start_line = m_checked_index;
+        printf("解析到一行HTTP信息: %s\n",text);
+        switch(m_check_state){
+            case CHECK_STATE_REQUESTLINE:
+            {
+                ret = parse_request_line(text);
+                if(ret==BAD_REQUEST){
+                    return BAD_REQUEST;
+                }
+                break;
+            }
+            case CHECK_STSTE_HEADER:
+            {
+                ret = parse_request_headers(text);
+                if(ret==BAD_REQUEST){
+                    return BAD_REQUEST;
+                }else if(ret==GET_REQUEST){
+                    return do_request(text); // 真正去解析请求
+                }
+                break;
+            }
+            case CHECK_STSTE_CONTENT:
+            {
+                ret = parse_request_content(text);
+                if(ret==GET_REQUEST){
+                    return do_request(text);
+                }
+                line_status = LINE_OPEN;
+                break;
+
+            }
+            default:
+            {
+                return INTERNAL_ERROR;
+            }
+        }
+        return NO_REQUEST;
+    }
+    
+
+}
+
+// 解析请求首行,最终要得到请求的方法和请求的目标url,HTTP的版本
+http_conn::HTTP_CODE http_conn::parse_request_line(char* text){
+    // 数据长这样：
+    // GET /index.html HTTP/1.1
+    m_url = strpbrk(text," \t");
+
+    *m_url++='\0';
+
+    char* method  =text;
+    if(strcasecmp(method,"GET")==0){
+        m_method=GET;
+    }else{
+        return BAD_REQUEST;
+    }
+
+    m_version = strpbrk(m_url," \t");
+    if(!m_version){
+        return BAD_REQUEST;
+    }
+    *m_version++='\0';
+
+    if(strcasecmp(m_version,"HTTP/1.1")!=0){
+        return BAD_REQUEST;
+    }
+
+    if(strncasecmp(m_url,"http://",7)==0){
+        m_url+=7;
+        m_url = strchr(m_url,'/');
+    }
+    if(!m_url||m_url[0]!='/'){
+        return BAD_REQUEST;
+    }
+    m_check_state = CHECK_STSTE_HEADER; // 请求首行解析完毕，将状态机状态设置为解析请求头
+
+    return NO_REQUEST;
+} 
+
+// 解析请求头
+http_conn::HTTP_CODE http_conn::parse_request_headers(char* text){
+
+} 
+
+// 解析请求请求体
+http_conn::HTTP_CODE http_conn::parse_request_content(char* text){
+
+} 
+
+// 获取具体的一行数据之后交给上面解析的函数
+http_conn::LINE_STATUS http_conn::parse_line(){
+    // 通过\r\n特殊标记来区分一行
+    char temp;
+    for(;m_checked_index<m_read_index;++m_checked_index){
+        temp = m_read_buf[m_checked_index];
+        if(temp=='\r'){
+            if (m_checked_index+1==m_read_index){
+                return LINE_OPEN;
+            }else if(m_read_buf[m_checked_index+1]=='\n'){
+                m_read_buf[m_checked_index++]='\0';
+                m_read_buf[m_checked_index++]='\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }else if(temp=='\n'){
+            if((m_checked_index>1)&&(m_read_buf[m_checked_index-1]=='\r')){
+                m_read_buf[m_checked_index-1]='\0';
+                m_read_buf[m_checked_index++]='\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+        return LINE_OPEN;
+    }
+} 
+
+// 具体的解析某一句
+ http_conn::HTTP_CODE http_conn::do_request(char* text){
+
+ }
--- http_conn.h
+++ http_conn.h
@@ -14,8 +14,10 @@
 #include<sys/mman.h>
 #include<stdarg.h>
 #include<error.h>
+#include<errno.h>
 #include"locker.h"
 #include<sys/uio.h>
+#include<string.h>
 
 class http_conn{
 public:
@@ -23,6 +25,40 @@ public:
     static int m_epollfd;
     // 统计用户的数量
     static int m_user_count;
+    // 读取数据的缓冲的大小
+    static const int READ_BUFFER_SIZE=2048;
+    // 写数据的缓冲的大小
+    static const int WRITRE_NUFFER_SIZE=2048;
+
+
+    // 有限状态机的状态，响应的状态信息
+    // HTTP请求方法，该项目只支持GET请求
+    enum METHOD{GET=0,POST,HEAD,PUT,DELETE,TRACE,OPTIONS,CONNECT};
+
+    /* 解析客户端发来的请求时主机自己的状态
+    CHECK_STATE_REQUESTLINE:当前正在分析请求
+    CHECK_STSTE_HEADER: 当前正在分析头部数据
+    CHECK_STSTE_CONTENT: 当前正在解析请求体
+    */
+    enum CHECK_STATE {CHECK_STATE_REQUESTLINE=0,CHECK_STSTE_HEADER,CHECK_STSTE_CONTENT};
+
+    /*服务器处理HTTP请求的可能结果
+    NO_REQUEST: 请求不完整，需要继续读取客户端发来的数据
+    GET_REQUEST: 获得了一个用户发来的完整的请求
+    BAD_REQUEST: 用户请求的语法错误
+    NO_RESOURCE:  服务器没有用户请求的资源
+    FORBIDDEN_REQUEST: 用户没有该资源的访问权限
+    FILE_REQUEST: 获取请求的文件成功
+    INTERNAL_ERROR: 服务器内部出错
+    CLOSED_CONNECTION: 客户端已经关闭连接
+    */
+    enum HTTP_CODE {NO_REQUEST=0,GET_REQUEST,BAD_REQUEST,NO_RESOURCE,FORBIDDEN_REQUEST,FILE_REQUEST,INTERNAL_ERROR,CLOSED_CONNECTION};
+
+    // 从状态机的三种可能状态，行的读取状态
+    // 1、读取到一个完整的行 2、行出错 3、行数据尚不完整
+    enum LINE_STATUS{LINE_OK=0, LINE_BAD, LINE_OPEN};
+
+
 
     // 构造与析构函数
     http_conn(){};
@@ -33,7 +69,7 @@ public:
     // 初始化新接受到的客户端的信息
     void init(int sockfd, const sockaddr_in &addr);
 
-    // 关闭连接
+    // 关闭连接'
     void close_conn();
 
     // 非阻塞读取数据
@@ -41,9 +77,46 @@ public:
     // 非阻塞写数据
     bool write();
 
+    // 解析请求信息
+    HTTP_CODE process_read();
+    HTTP_CODE parse_request_line(char* text); // 解析请求首行
+    HTTP_CODE parse_request_headers(char* text); // 解析请求头
+    HTTP_CODE parse_request_content(char* text); // 解析请求请求体
+
+    LINE_STATUS parse_line(); // 获取具体的一行数据之后交给上面解析的函数
+
+    
+
 private:
     int m_sockfd; // 该http连接的socket
     sockaddr_in m_address; // 通信的socket地址
+    char m_read_buf[READ_BUFFER_SIZE];  // 读缓冲区
+    int m_read_index; // 读缓冲区中已经读取进来的最后一个字节的下标，因为可能一次读不完，缓冲区装不下，需要来回运
+    char m_write_buf[WRITRE_NUFFER_SIZE];
+
+    int m_checked_index; // 当前正在读的字符在缓冲区中的位置
+    int m_start_line; // 当前正在解析的行的起始位置
+
+    char* m_url;  // 请求的目标文件的文件名
+    char* m_version; // 请求的http版本
+    METHOD m_method; // 请求方法
+    char* m_host; // 主机名
+    bool m_linger; //是否保持连接
+    
+
+
+
+
+    CHECK_STATE m_check_state;  // 主状态机当前的状态
+
+    void init();// 初始化连接其余的数据
+
+    // 或许接下来要读取的一行数据
+    char* get_line(){return m_read_buf+m_start_line;}
+
+    // 具体处理数据
+    HTTP_CODE do_request(char* text);
+
 };
 
 #endif
